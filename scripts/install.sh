59e8252063ed33f054250fbc406ebfcbb10338eb
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-05-03 00:14:07 -0700

    specify pip/python venv location

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-echo "Checking for root"
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 apt install git -y
 git clone https://github.com/dazemc/flask_networkmanager.git
-mv flask_networkmanager wipi_portal && cd wipi_portal
+mv flask_networkmanager wipi_portal
+cd wipi_portal
 mkdir .venv
+echo "Creating python venv..."
 python3 -m venv .venv/
-source .venv/bin/activate
-pip install flask, flask_cors
-python app.py
+echo "Installing python packages..."
+.venv/bin/pip install -r requirements.txt
 exit
---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+.venv/bin/python app.py
+exit
