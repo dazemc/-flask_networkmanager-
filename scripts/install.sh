c4de5810362106d7c006dcc9196d6bb79c661a3d
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-05-05 19:27:10 -0700

    remove previous download++

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -f master.zip
+rm -f master.zip && rm -rf wipi_portal
 wget https://github.com/dazemc/flask_networkmanager/archive/refs/heads/master.zip
 unzip master.zip
 mv flask_networkmanager-master wipi_portal
